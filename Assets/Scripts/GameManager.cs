9720a25bac38d857a86cefd2443371c08186cc94
Alcatraz Lynn <lym0104@126.com>
2021-04-23 23:17:53 +0900

    Added Menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,8 @@ public class GameManager : MonoBehaviour
 {
     public NormalAgent agentA;
     public NormalAgent agentB;
+    public NormalAgent agentC;
+    public NormalAgent agentD;
     
 
     // Update is called once per frame
