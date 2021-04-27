f16cc2c777281b58567b137a51f4f4bfa5a43f30
Alcatraz Lynn <lym0104@126.com>
2021-04-27 15:55:51 +0900

    Add Main Menu and camera settings

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 5f;
+    public Vector3 offset;
+
+    void FixedUpdate(){
+        Vector3 targetPosition = target.position + offset;
+        Vector3 smoothedPostion = Vector3.Lerp(transform.position, targetPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPostion;
+    }
+}
