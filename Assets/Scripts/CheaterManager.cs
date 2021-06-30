4fb95df2029c66c531dbb28e5c4795ba84f1fee0
Alcatraz Lynn <lym0104@126.com>
2021-06-30 18:59:40 +0900

    Complete
    Finally...

--- Assets/Scripts/CheaterManager.cs
+++ Assets/Scripts/CheaterManager.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.MLAgents;
+using Unity.Barracuda;
 
 public class CheaterManager : MonoBehaviour
 {
+    public NNModel cheaterBrain;
     bool hasCheater = false;
     NormalAgentGame[] agents;
     int index;
@@ -16,6 +19,7 @@ public class CheaterManager : MonoBehaviour
             agents = FindObjectsOfType<NormalAgentGame>();
             index = Random.Range(0, agents.Length - 1);
             agents[index].isCheating = true;
+            agents[index].SetModel("NormalAgent", cheaterBrain);
         }
     }
 }
